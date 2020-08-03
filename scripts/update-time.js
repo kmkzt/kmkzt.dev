f9c4d107fd148fcd5ff90808c7374dd18c8756b9
kmkzt <info.pscreator@gmail.com>
2020-08-03 23:46:57 +0900

    feat: add update-time scripts

---
+++ scripts/update-time.js
@@ -0,0 +1,14 @@
+#!/usr/bin/env node
+const matter = require('gray-matter')
+const { join } = require('path')
+const { readFile, writeFileSync } = require('fs')
+
+const updateFile = join(process.cwd(), process.argv[2])
+
+readFile(updateFile, (err, md) => {
+  const { content, data } = matter(md)
+  writeFileSync(
+    updateFile,
+    matter.stringify(content, { ...data, updatedAt: new Date().toISOString() })
+  )
+})
