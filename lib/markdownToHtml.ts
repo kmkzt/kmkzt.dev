601865b292420db9e888f37bd6c0f4a2138b190b
kmkzt <info.pscreator@gmail.com>
2020-08-08 23:12:35 +0900

    feat: add remark-basepath

--- lib/markdownToHtml.ts
+++ lib/markdownToHtml.ts
@@ -1,7 +1,5 @@
 import remark from 'remark'
-import { Plugin } from 'unified'
-import { Node } from 'unist'
-import visit from 'unist-util-visit-parents'
+import basepath from 'remark-basepath'
 // @ts-expect-error
 import html from 'remark-html'
 // @ts-expect-error
@@ -10,35 +8,14 @@ import highlight from 'remark-highlight.js'
 const isRelativeImage = (str: string) =>
   str.match(/\.\/(.*?.(svg|png|jpg|jpeg|gif)$)/)
 
-interface ImageNode extends Node {
-  type: 'image'
-  title: any
-  url: string
-  alt: string
-  position: any
-}
-const baseImagePath: Plugin<[string], Partial<remark.RemarkOptions>> = (
-  base
-) => (tree) =>
-  visit(tree, 'image', (node: ImageNode, parents) => {
-    // console.log('node: ', node)
-    // console.log('parents: ', parents)
-    const image = isRelativeImage(node.url)
-    if (image) {
-      const siblings: any = parents[parents.length - 1].children
-      siblings[siblings.indexOf(node)] = {
-        ...node,
-        url: `${base}/${image[1]}`,
-      }
-    }
-  })
-
 export default async function markdownToHtml(
   markdown: string,
   basePath: string
 ): Promise<string> {
   const result = await remark()
-    .use(baseImagePath, basePath)
+    .use(basepath, {
+      basePath,
+    })
     .use(html)
     .use(highlight)
     .process(markdown)
